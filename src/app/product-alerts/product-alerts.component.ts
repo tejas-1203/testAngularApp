d3c3c4877a902c0e86d4e11c6864cd010bff444f
tejas-1203 <132695771+tejas-1203@users.noreply.github.com>
2023-05-05 16:53:04 -0400

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product} from '../products'
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class ProductAlertsComponent {
 
+  @Input() product!: Product;
 }
